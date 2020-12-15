80c46c0120851db6a1463abadcab410569f9f713
David Backeus <duztdruid@gmail.com>
2020-12-15 19:27:43 +0100

    Use checksum for cache busting

--- app/models/buildless_cache.rb
+++ app/models/buildless_cache.rb
@@ -3,9 +3,9 @@ module BuildlessCache
     @modules ||= Dir.glob("public/buildless/**/*.js").each_with_object({}) do |path, hash|
       name = path.delete_prefix("public/buildless/").delete_suffix(".js")
       relative_path = path.delete_prefix("public")
-      last_modified_at = File.mtime(path).strftime("%Y-%m-%d_%H-%M-%S")
+      checksum = Digest::MD5.file(path).hexdigest
 
-      hash[name] = "#{relative_path}?last_modified_at=#{last_modified_at}"
+      hash[name] = "#{relative_path}?checksum=#{checksum}"
     end
   end
 
