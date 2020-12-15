808679fe85c251e68d2e493427815107b4a6ceda
David Backeus <duztdruid@gmail.com>
2020-12-15 19:05:08 +0100

    Query string based cache busting for buildless files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     nm = (meters / 1852).round
     "#{nm} NM"
   end
+
+  def buildless_module(name)
+    url = BuildlessCache.modules[name]
+
+    raise "module '#{name}' not found" unless url
+
+    %(<script type="module" src="#{url}"></script>).html_safe
+  end
 end
