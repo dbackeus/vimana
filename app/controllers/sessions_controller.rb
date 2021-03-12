3d95abac97be15c8e4c1e75971ed8a4627353ea3
David Backeus <duztdruid@gmail.com>
2021-03-12 16:24:51 +0100

    Add dev login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
     redirect_to games_path if user_signed_in?
   end
 
+  if Rails.env.development?
+    def dev_login
+      sign_in User.first
+
+      redirect_to games_path
+    end
+  end
+
   def destroy
     sign_out
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
 
   resource :session, only: %i[create destroy]
+
+  get "dev_login", to: "sessions#dev_login" if Rails.env.development?
+
   resources :games
   resources :airports, only: :index
   resources :missions, only: :create
